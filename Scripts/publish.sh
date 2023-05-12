b53673674394684d727de740830286ed2d0df39d
wellWINeo <stepan@uspenskiy.su>
2023-05-12 19:48:12 +0300

    fix publish script and update version

--- Scripts/publish.sh
+++ Scripts/publish.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-PROJECT_DIR=./StupidMapper
+# prelude
+PROJECT_NAME=StupidMapper
+PROJECT_DIR=./$PROJECT_NAME
 
-dotnet clean $PROJECT_DIR
-dotnet pack --configuration Release $PROJECT_DIR
+cd $PROJECT_DIR
 
-NUPKG_FILE=$(cd $PROJECT_DIR && find . -name "*.nupkg" && cd ../)
+# clean all
+dotnet clean
+rm -rf bin
+rm -rf obj
 
+# pack .nupkg
+dotnet pack --configuration Release
+NUPKG_FILE=$(find . -name "$PROJECT_NAME.**.nupkg")
+
+# publish
 dotnet nuget push $NUPKG_FILE --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json
